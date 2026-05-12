68f20f777655da030f24fbfb806b3867e193824d
VA-GS <voxeladventure.gs@gmail.com>
2026-05-12 21:33:38 +0200

    FL_INDEX_MAX = 36

--- tlsf/tlsf.c
+++ tlsf/tlsf.c
@@ -239,7 +239,7 @@ enum tlsf_private
 	** TODO: We can increase this to support larger sizes, at the expense
 	** of more overhead in the TLSF structure.
 	*/
-	FL_INDEX_MAX = 32,
+	FL_INDEX_MAX = 36,
 #else
 	FL_INDEX_MAX = 30,
 #endif
